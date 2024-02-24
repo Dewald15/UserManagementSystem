bb6f367909800f2dd86cc976acbeea5431b2d91b
Dewald15 <vdbergdewald@gmail.com>
2024-02-24 14:22:59 +0200

    changed database connection properties

--- src/main/webapp/Project/Project/ConnectionProvider.java
+++ src/main/webapp/Project/Project/ConnectionProvider.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 public class ConnectionProvider {
     public static Connection getConnection(){
         try {
-//            String url = "jdbc:mysql://localhost:3306/uuu"; // Establish a connection to the MySQL database
-//            String username = "root";
-//            String password = "160991";
+            String url = "jdbc:mysql://localhost:3306/uuu"; // Establish a connection to the MySQL database
+            String username = "root";
+            String password = "160991";
             Class.forName("com.mysql.cj.jdbc.Driver"); // Load the MySQL JDBC driver
-            String url = "jdbc:mysql://sql8.freesqldatabase.com:3306/sql8684532"; // Establish a connection to the MySQL database
-            String username = "sql8684532";
-            String password = "vRNNaS4tDw";
+//            String url = "jdbc:mysql://sql8.freesqldatabase.com:3306/sql8684532"; // Establish a connection to the MySQL database
+//            String username = "sql8684532";
+//            String password = "vRNNaS4tDw";
             Connection connection = DriverManager.getConnection(url, username, password); // Perform database operations using the 'connection' object
             return connection;
         } catch (ClassNotFoundException | SQLException e) {
